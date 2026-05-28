d723a258c1759b6ac3c59fc67bf5100c23949b42
alsptr06 <alwisyah61@gmail.com>
2026-05-28 18:54:22 +0700

    Delete Modul 9/THT directory

--- Modul 9/THT/THT/src/CustomerService.java
+++
@@ -1,5 +0,0 @@
-public class CustomerService { //class untuk menangani keluhan nasabah
-    public void laporkanKeluhan(String keluhan) { //method untuk menerima keluhan nasabah dan menampilkannya
-        System.out.println("[CS] Keluhan diterima: " + keluhan);
-    }
-}
--- Modul 9/THT/THT/src/Otorisasi.java
+++
@@ -1,3 +0,0 @@
-public interface Otorisasi { //interface untuk otorisasi nasabah, misalnya verifikasi PIN
-    boolean verifikasiPIN(String pin); //method untuk memverifikasi PIN yang dimasukkan oleh nasabah, akan diimplementasikan di kelas Rekening
-}
